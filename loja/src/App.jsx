dac0950395588e33b196286915533ae6c859d04e
MATHEUS DE ALMEIDA VILLEGAS REIS <CG3037819@car.ifsp>
2026-03-31 08:33:38 -0300

    Banner colocado

--- loja/src/App.jsx
+++ loja/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Topo from './components/Topo'
+import Capa from './components/Capa'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
       <header>
         <Topo/>
       </header>
+      <main> <Capa></Capa></main>
     </div>
   )
 }
